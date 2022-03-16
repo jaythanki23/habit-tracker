e1edec39b09d90b0adeb37c077ab850222bd6429
Jay Thanki <jaythanki7000@gmai.com>
2022-03-16 18:00:59 +0530

    Created the UI of the Home page

---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CheckboxForm from '../CheckboxForm';
+
+const Home = () => {
+  return (
+    <div>
+      <CheckboxForm />
+    </div>
+  )
+}
+
+export default Home
