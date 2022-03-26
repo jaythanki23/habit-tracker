d135f93d2b378f2ff76f11567c837cd071d391d8
Jay Thanki <jaythanki7000@gmai.com>
2022-03-26 14:43:22 +0530

    Created a route

---
+++ backend/routes/HabitRoutes.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.status(200).json({ message: "Your habits" });
+});
+
+// router.post('/', (req, res) => {
+//   res.status(200).json({ message: "Created a habit" });
+// })
+
+export { router };
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
+import { router as habits } from '../backend/routes/HabitRoutes.js';
 
-const port = 5000;
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+// Init Middleware
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//   res.send("Welcome to my app");
+// })
+
+app.use('/dashboard', habits);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
